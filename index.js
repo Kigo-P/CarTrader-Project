2d5a9d0900da67f067b17d8fc6384a8c08d2e312
Kigo-P <peterkigo33@gmail.com>
2024-07-19 20:52:49 +0300

    Updated changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     //creating a variable for div
-    
+    let profitArray = []// used to caltulate the profit
 const displayCars = document.getElementById("displayCars")
-let profitArray = []
+
 //creating a function that fetches all the cars and puts them in specific divs
 function viewCars(){
     
@@ -29,6 +29,7 @@ function viewCars(){
                        //looping through the available car objects to get one object using the filter method
                     availableCars.filter(car=> {
                         if((car.make).toLowerCase() === searchInput.toLowerCase()){
+                            showCar.innerHTML = ``
                             showCar.innerHTML = `
                             <img src="${car.image}" alt="">
                             <h2>Make: ${car.make}</h2>
@@ -41,7 +42,7 @@ function viewCars(){
                         }
                     })
                 })
-                searchInput = ''
+                
             })
                
          }
@@ -85,57 +86,54 @@ function viewCars(){
             sellInput.setAttribute("required", "");
             sold.addEventListener("click",function(event){
                 event.preventDefault()
+                event.stopPropagation()
                 function getInput(){
                     //creating a button that submits the input above and automaically calculates the profit 
                     const submitInput = document.createElement("button")
                     submitInput.textContent = "Submit"
                     submitInput.addEventListener("click", function(event){
                             event.preventDefault()
-                       //creating a variables and elements to a table 
-                    const tr1 = document.createElement("tr")
-                    const td1 = document.createElement("td")
-                    const td2 = document.createElement("td")
-                    const td3 = document.createElement("td")
-                    const td4 = document.createElement("td")
-                    const td5 = document.createElement("td")
-                    const td6 = document.createElement("td")
-                    const td7 = document.createElement("td")
-                    const td8 = document.createElement('td')
-                    const td9 = document.createElement('td')
-                    const td10 = document.createElement('td')
-                    const deleteButton = document.createElement("button")
-                    deleteButton.textContent = "delete"
-                    td7.appendChild(deleteButton)  
-                    const tr2 = document.getElementById("tr2")  
-                     //transfering the data from the divs to the table once the vehicle has been sold
-                    td1.textContent = `${car.make}`
-                    td2.textContent = `${car.model}`
-                    td3.textContent = `${car.engine_no}`
-                    //creating the image that should be appended to the table
-                    let image1 = document.createElement('img')
-                    image1.src = car.image
-                    image1.style= "height: 200px; border-radius: 15px;"
-                    td4.appendChild(image1)
-                    td5.textContent = `${car.price}`
-                    td6.textContent = `${car.description}`
-                    // let totalProfit = 0
-                    //calculating the profit 
-                    const sellingPrice = parseInt(sellInput.value)
-                    const buyingPrice = parseInt(car.price.split(",").join(""))
-                    
-                    profit = sellingPrice - buyingPrice
-                    td8.textContent = sellingPrice
-                    profitArray.push(profit)
-                    console.log(profitArray)
-                    td9.textContent = profit
-                    
-                    // // to calculate the total profit:
-                    // totalProfit += profit
+                            
+                            div1.remove()//removing the item on the div and taking it to the table  
+                        //creating a variables and elements to a table 
+                        const tr1 = document.createElement("tr")
+                        const td1 = document.createElement("td")
+                        const td2 = document.createElement("td")
+                        const td3 = document.createElement("td")
+                        const td4 = document.createElement("td")
+                        const td5 = document.createElement("td")
+                        const td6 = document.createElement("td")
+                        const td7 = document.createElement("td")
+                        const td8 = document.createElement('td')
+                        const td9 = document.createElement('td')
+                        const td10 = document.createElement('td')
+                        const deleteButton = document.createElement("button")
+                        deleteButton.textContent = "delete"
+                        td7.appendChild(deleteButton)  
+                        const tr2 = document.getElementById("tr2")  
+                        //transfering the data from the divs to the table once the vehicle has been sold
+                        td1.textContent = `${car.make}`
+                        td2.textContent = `${car.model}`
+                        td3.textContent = `${car.engine_no}`
+                        //creating the image that should be appended to the table
+                        let image1 = document.createElement('img')
+                        image1.src = car.image
+                        image1.style= "height: 200px; border-radius: 15px;"
+                        td4.appendChild(image1)
+                        td5.textContent = `${car.price}`
+                        td6.textContent = `${car.description}`
+                   
+                        //calculating the profit 
+                        
+                        const sellingPrice = parseInt(sellInput.value)
+                        const buyingPrice = parseInt(car.price.split(",").join(""))
+                        profit = sellingPrice - buyingPrice
+                        td8.textContent = sellingPrice
+                        profitArray.push(profit)
+                        
+                        td9.textContent = profit
                     // td10.textContent = ''
-                    // td10.textContent = profit
-
-                    
-
+                    td10.textContent = profit
                      //appending the elements to the tbody and trow
                      tr1.appendChild(td1)
                      tr1.appendChild(td2)
@@ -150,14 +148,24 @@ function viewCars(){
                      tbody.appendChild(tr2)
                      tr2.appendChild(td10)
                     })
+                    let totalProfit = 0
+                    function sumOfTotal(){
+                        for(let i = 0; i <= profitArray.length-1; i++){
+                            totalProfit+=profitArray[i]
+                        }
+                        
+                        console.log(totalProfit)
+                    }sumOfTotal()
                     //disabling the sold button so that when it is clicked, it does not display multiple times
                     sold.disabled = true
                     //appending the submit Input
                     div1.appendChild(submitInput)
+                    postSoldCar(car.id)
                 }
                 getInput()
             div1.appendChild(sellInput)
-            updateServer(car.id,sellingPrice,profit)
+            
+            // updateServer(car.id)
             })
             //adding an event listener to the delete button whereby it will remove the image 
             deleteInput.addEventListener("click", function(){
@@ -174,31 +182,36 @@ function viewCars(){
                 .then(deleteImage => console.log(deleteImage))
             })
         }
-        //function to update the profit,selling price to the server
-        function updateServer(carId,sellingPrice,profit){
-            let newServerData ={
-                selling_price : sellingPrice,
-                profit: profit,
-                sold: true
-            }
-            fetch(`http://localhost:3000/soldCars${carId}`,{
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                    "CAccept": "application/json"
-                },
-                body: JSON.stringify(newServerData)
-            })
-            .then(response => response.json())
-            .then(updateNewData =>{
-                if(updateNewData.ok){
-                    return updateNewData
-                }
-                else{
-                    return "error"
-                }
-            })
-        }
+        //creating postSoldCar function that fetches the data from the div and takes it to the table 
+        function postSoldCar(carId){ 
+            console.log(carId,"carId")
+            //targeting the value of the input given on the form
+            const make = document.getElementById("make").value;
+            const model = document.getElementById("model").value;
+            const engineno = document.getElementById("engineno").value;
+            const image = document.getElementById("image").value;
+            const price = document.getElementById("price").value;
+            const description = document.getElementById("description").value;
+           //creating a javascript object
+           let carData = {
+               make: make,
+               model: model,
+               engine_no: engineno,
+               image: image,
+               price: price,
+               description: description
+           }
+           //using fetch method to post the car in the soldCars Database
+           fetch(`http://localhost:3000/soldCars/${carId}`, {
+               method: "POST",
+               headers: {
+                   "Content-Type" : "application/json",
+                   "Accept" : "application/json",
+               },
+               body: JSON.stringify(carData)
+           })
+           .then(response => console.log(response))
+        }  
         
         //creating an event listener on the submit button so that when the input is submitted, it fetches the data using post method and displays it to the user and database as well
         //assigning a variable to our form
